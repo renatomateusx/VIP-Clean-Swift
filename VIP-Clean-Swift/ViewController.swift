5b9b31642e518af9fc492d4ca8ba78b27144f6ba
Renato Santos <renatomateusx@gmail.com>
2021-10-12 16:41:21 -0300

    Initial Commit

---
+++ VIP-Clean-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIP-Clean-Swift
+//
+//  Created by Renato Mateus De Moura Santos on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
